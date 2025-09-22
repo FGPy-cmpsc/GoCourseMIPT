b2e005f1d68dd6dacba63f76be9759418a37a01d
Стародубов Александр Юрьевич <71456730+FGPy-cmpsc@users.noreply.github.com>
2025-09-22 14:58:03 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"hash/fnv"
 )
 
+type Book struct {
+	BookName string
+	Author   string
+	Edition  string
+}
+
+func (book Book) GetInfo() string {
+	return "book name: " + book.BookName + ", author: " + book.Author + ", edition: " + book.Edition
+}
+
 type Library struct {
-	Storage   map[int64]string
+	Storage   map[int64]Book
 	GetIdFunc func(str string) int64
 }
 
@@ -22,16 +32,17 @@ func HashStr(str string) int64 {
 	return hash
 }
 
-func (library *Library) AddBook(str string) {
-	new_key := library.GetIdFunc(str)
-	library.Storage[new_key] = str
+func (library *Library) AddBook(name, author, edit string) {
+	new_key := library.GetIdFunc(name)
+	library.Storage[new_key] = Book{name, author, edit}
 }
 
-func (library *Library) GetBook(str string) string {
+func (library *Library) GetBook(str string) Book {
 	book_key := library.GetIdFunc(str)
 	book, ok := library.Storage[book_key]
 	if !ok {
-		return "Error: No book named " + str
+		fmt.Println("Error: No book named " + str)
+		return book
 	}
 	delete(library.Storage, book_key)
 	return book
@@ -44,10 +55,10 @@ func AdvancedHash(str string) int64 {
 }
 
 func main() {
-	books1 := []string{"War and Peace", "Anna Karenina", "Doctor Zhivago", "The Brothers Karamazov"}
-	library := &Library{Storage: make(map[int64]string), GetIdFunc: HashStr}
-	for _, book_name := range books1 {
-		library.AddBook(book_name)
+	books1 := [][]string{{"War and Peace", "Tolstoy", "ECSMO"}, {"Anna Karenina", "Tolstoy", "AST"}, {"Doctor Zhivago", "Pasternak", "AST"}, {"The Brothers Karamazov", "Dostoyevsky", "EBook"}}
+	library := &Library{Storage: make(map[int64]Book), GetIdFunc: HashStr}
+	for _, book_info := range books1 {
+		library.AddBook(book_info[0], book_info[1], book_info[2])
 	}
 	books2 := []string{"War and Peace", "Crime and Punishment", "Doctor Zhivago", "The Brothers Karamazov"}
 	for _, book_name := range books2 {
@@ -55,12 +66,12 @@ func main() {
 	}
 
 	library.GetIdFunc = AdvancedHash
-	books3 := []string{"A Hero Of Our Time", "The Master and Margarita", "Dead Souls"}
-	for _, book_name := range books3 {
-		library.AddBook(book_name)
+	books3 := [][]string{{"A Hero Of Our Time", "Lermontov", "Azbuka"}, {"The Master and Margarita", "Bulgakov", "AST"}, {"Dead Souls", "Gogol", "Myf"}}
+	for _, book_info := range books3 {
+		library.AddBook(book_info[0], book_info[1], book_info[2])
 	}
 	books4 := []string{"Crime and Punishment", "The Master and Margarita"}
 	for _, book_name := range books4 {
-		fmt.Println(library.GetBook(book_name))
+		fmt.Println(library.GetBook(book_name).GetInfo())
 	}
 }
